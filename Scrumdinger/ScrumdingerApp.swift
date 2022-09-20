1bf051a097cf9f2345c2329bcab524dfbd1940be
Ni Qiang <ni.qiang@imt-atlantique.net>
2022-09-20 14:03:52 +0200

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Ni QIANG on 20/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
